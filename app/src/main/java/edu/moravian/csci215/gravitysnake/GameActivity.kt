66f80ccc3f0b3d919d8bf117fb8d3c802716972b
Reed Sturza <sturzar@moravian.edu>
2022-10-19 15:08:42 -0400

    slider and gameActivity

--- app/src/main/java/edu/moravian/csci215/gravitysnake/GameActivity.kt
+++ app/src/main/java/edu/moravian/csci215/gravitysnake/GameActivity.kt
@@ -1,11 +1,15 @@
 package edu.moravian.csci215.gravitysnake
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.material.slider.Slider
 import edu.moravian.csci215.gravitysnake.databinding.ActivityGameBinding
 
 
@@ -21,6 +25,16 @@ import edu.moravian.csci215.gravitysnake.databinding.ActivityGameBinding
  */
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
+    /** The sensor manager service that gives access to sensors and listening to their events */
+    private var sensorManager: SensorManager? = null
+    /** The gravity sensor we are using in the app */
+    private var gravitySensor: Sensor? = null
+    /** The snakeGameView for this app */
+    private lateinit var snakeGameView: SnakeGameView
+    /** the TextView for the chosen difficulty */
+    private lateinit var difficultyResult: TextView
+    /** The snakeGameView for this app */
+    private lateinit var difficultySlider: Slider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,14 +44,40 @@ class GameActivity : AppCompatActivity() {
         // Force the game to be fullscreen
         hideSystemUI()
 
+        //register UI elements for the slider and the chosen difficulty
+        difficultyResult = findViewById(R.id.difficultyResult)
+        difficultySlider = findViewById(R.id.difficultySlider)
+
+
         // TODO: set the difficulty of the snake game view
+        //snakeGameView.setDifficulty()
 
         // TODO: setup the sensors (use the gravity sensor)
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        gravitySensor = sensorManager?.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        snakeGameView = findViewById(R.id.snakeGameView)
     }
 
 
     // TODO: properly setup sensor event listeners (the listener is the snake game view)
+    /** When the activity resumes, we start listening to the sensor. */
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(snakeGameView, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
 
+    /** When the activity pauses, we stop listening to the sensor. */
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(snakeGameView)
+    }
+
+    /** When the activity is destroyed, we cleanup our variables. */
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager = null
+        gravitySensor = null
+    }
 
     ///// Don't worry about the rest of this code - it deals with making a fullscreen app /////
     /** Hides the system UI elements for the app, making the app full-screen.  */
