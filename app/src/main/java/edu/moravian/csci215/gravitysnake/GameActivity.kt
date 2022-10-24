2e55d0b67cab2b4cccbb47edfb379d1a72e5640f
David Olsakowski <olsakowskid@moravian.edu>
2022-10-23 21:00:21 -0400

    welcome activity + updates

--- app/src/main/java/edu/moravian/csci215/gravitysnake/GameActivity.kt
+++ app/src/main/java/edu/moravian/csci215/gravitysnake/GameActivity.kt
@@ -1,9 +1,10 @@
 package edu.moravian.csci215.gravitysnake
 
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -31,16 +32,15 @@ import edu.moravian.csci215.gravitysnake.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
+
     /** The sensor manager service that gives access to sensors and listening to their events */
     private var sensorManager: SensorManager? = null
+
     /** The gravity sensor we are using in the app */
     private var gravitySensor: Sensor? = null
+
     /** The snakeGameView for this app */
     private lateinit var snakeGameView: SnakeGameView
-    /** the TextView for the chosen difficulty */
-    private lateinit var difficultyResult: TextView
-    /** The snakeGameView for this app */
-    private lateinit var difficultySlider: Slider
 
     /** enum class to convert the value of the difficultySlider to a difficulty setting */
     enum class DifficultyFormat(val difficultyInt: Int) {
@@ -60,20 +60,6 @@ class GameActivity : AppCompatActivity() {
         // Force the game to be fullscreen
         hideSystemUI()
 
-        //register UI elements for the slider and the chosen difficulty
-        difficultyResult = findViewById(R.id.difficultyResult)
-        difficultySlider = findViewById(R.id.difficultySlider)
-        // defaults the difficulty to BEGINNER
-        difficultyResult.text = DifficultyFormat.findByValue(0).toString()
-
-        // TODO: set the difficulty of the snake game view
-        // add Change listener for the difficultySlider to show on TextView and change the Difficulty
-        difficultySlider.addOnChangeListener {
-                _, value, _ -> difficultyResult.text = value.toDouble().toString()
-                // changes the difficulty of the game when the slider value is changed by th user
-                snakeGameView.setDifficulty(value.toInt())
-        }
-
         // TODO: setup the sensors (use the gravity sensor)
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         gravitySensor = sensorManager?.getDefaultSensor(Sensor.TYPE_GRAVITY)
@@ -81,12 +67,15 @@ class GameActivity : AppCompatActivity() {
 
     }
 
-
     // TODO: properly setup sensor event listeners (the listener is the snake game view)
     /** When the activity resumes, we start listening to the sensor. */
     override fun onResume() {
         super.onResume()
-        sensorManager?.registerListener(snakeGameView, gravitySensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager?.registerListener(
+            snakeGameView,
+            gravitySensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
     }
 
     /** When the activity pauses, we stop listening to the sensor. */
@@ -106,7 +95,8 @@ class GameActivity : AppCompatActivity() {
     /** Hides the system UI elements for the app, making the app full-screen.  */
     private fun hideSystemUI() {
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
-        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
         // Keep the screen on as well
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
